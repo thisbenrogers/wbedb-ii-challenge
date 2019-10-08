f48f5e6b6bacdfddcb0e82c744845831fa5cd8d8
Ben <thisbenrogers@gmail.com>
2019-10-08 17:04:15 -0500

    :card_file_box: Adds GET for cars

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
-// const carsRouter = require('../cars/cars-router');
+const carsRouter = require('../cars/cars-router');
 
 const server = express();
 
 server.use(express.json());
 
-// server.use('/api/cars', carsRouter);
+server.use('/api/cars', carsRouter);
 
 module.exports = server;
--- cars/cars-router.js
+++ cars/cars-router.js
@@ -8,7 +8,13 @@ const db = knex(knexConfig.development);
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  
+  db('cars')
+    .then(cars => {
+      res.status(200).json(cars);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to retrieve cars' });
+    });
 });
 
 router.post('/', (req, res) => {
